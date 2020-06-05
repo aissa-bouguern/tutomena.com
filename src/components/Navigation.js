b0a3602f9894f9695f7d4cebd017699aa3eaabe6
joalharbi <66185129+joalharbi@users.noreply.github.com>
2020-06-05 19:39:29 +0300

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -39,8 +39,8 @@ export default class Navigation extends Component {
         <div className="nav-container">
           <div className="brand">
             <Link to="/">
-              <img src={tutomena} className="favicon" alt="مدونة توتومينا" />
-              <span className="text">مدونة توتومينا</span>
+              <img src={tutomena} className="favicon" alt="مجتمع روف" />
+              <span className="text">مجتمع روف</span>
             </Link>
           </div>
           <div className="links">
