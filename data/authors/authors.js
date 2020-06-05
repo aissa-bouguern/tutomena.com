aa34e6350ab08a64bf37408c2410ca588846b191
joalharbi <66185129+joalharbi@users.noreply.github.com>
2020-06-05 19:14:23 +0300

    Update authors.js

--- data/authors/authors.js
+++ data/authors/authors.js
@@ -1,7 +1,7 @@
 import photo_abdulrhman from './avatars/abdulrhman.png';
 
 const abdulrhman = {
-  name: 'عيسى محمد علي',
+  name: 'عبدالرحمن خالد',
   photo: photo_abdulrhman,
   bio:
     'المدون الرئيسي هنا ، أساعدك في كل شيء تريدة محب للمساعدة وصاحب متجر روف هوست وسلسلة روف  ، مطور قديم لبعض برامج الهواتف ومواقع الويب ،  طالب رياضيات',
