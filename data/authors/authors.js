e3e12dcff197a2affd26eb889557d544b66fc8e0
joalharbi <66185129+joalharbi@users.noreply.github.com>
2020-06-05 18:09:46 +0300

    Update authors.js

--- data/authors/authors.js
+++ data/authors/authors.js
@@ -2,15 +2,15 @@ import photo_abdulrhman from './avatars/abdulrhman.png';
 
 const abdulrhman = {
   name: 'عيسى محمد علي',
-  photo: photo_aissa,
+  photo: photo_abdulrhman,
   bio:
-    'مغربي من أصول أمازيغية، مطور ويب مستقل متخصص في الواجهات الأمامية، أحب التدوين وإغناء المحتوى التقني للغة الضاد وهذا كان السبب الرئيسي في إنشائي لمدونة توتومينا.',
+    'المدون الرئيسي هنا ، أساعدك في كل شيء تريدة محب للمساعدة وصاحب متجر روف هوست وسلسلة روف  ، مطور قديم لبعض برامج الهواتف ومواقع الويب ،  طالب رياضيات',
   website: '',
   social: {
     facebook: '',
-    twitter: '',
-    github: 'https://github.com/aissa-bouguern',
-    linkedin: 'https://linkedin.com/in/bouguern'
+    twitter: 'https://twitter.com/1roov',
+    github: '',
+    linkedin: ''
   }
 };
 export default { abdulrhman };
